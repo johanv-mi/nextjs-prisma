7eb107ed2206e8298063a82a6b4b47d7693baec8
johanv-mi <johan.victorin@medieinstitutet.se>
2025-03-11 10:27:31 +0100

    render posts on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h2>Home</h2>;
+import { db } from "@/prisma/db";
+
+export default async function HomePage() {
+  const posts = await db.post.findMany();
+  return (
+    <main className="grid gap-8 p-4">
+      {posts.map((post) => (
+        <article key={post.id} className="grid p-2 rounded hover:bg-slate-200">
+          <span className="text-slate-700">
+            {post.createdAt.toLocaleDateString()}
+          </span>
+          <h2 className="text-2xl">{post.title}</h2>
+          <p className="text-slate-800">{post.content}</p>
+        </article>
+      ))}
+    </main>
+  );
 }
